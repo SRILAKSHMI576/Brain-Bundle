be05b9139bc8981b6698c5c2e7b1d695a0cc0ed2
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2020-03-11 12:39:35 +0530

    Typo changes

--- projectEuler/multipleOf3And5.js
+++ projectEuler/multipleOf3And5.js
@@ -2,12 +2,12 @@
 
 function multipleOf3And5(num){
 	let sum = 0;
-	for(index = 1; index <= num; index++){
+	for(let index = 1; index < num; index++){
 		if(index % 3 == 0 || index % 5 == 0 ){
 		sum += index
 		}
 	}
-	console.log(sum)
+	return sum;
 }
 const result = multipleOf3And5(1000)
 console.log(result)
